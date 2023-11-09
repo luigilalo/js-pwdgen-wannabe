1e1b9ecc0a61a670374d6835021e957dbd835a5f
luigilalo <luigilaloe1@gmail.com>
2023-11-09 15:33:15 +0100

    Improvements

--- js/main.js
+++ js/main.js
@@ -7,6 +7,15 @@ let color = prompt("Qual è il tuo colore preferito?")
 let number1 = prompt("Dimmi un primo numero")
 let number2 = prompt("Dimmi un secondo numero")
 
+// Converto variabili NUMBER (che erano testi per via del prompt) in NUMERI
+number1 = parseInt(number1);
+number2 = parseInt(number2);
+
+
+// OPERAZIONE 
+
+let DivisionBetweenNumbers = number1 / number2
+let password = name + surname + color
 
 
 
@@ -18,6 +27,8 @@ Nome e Cognome: ${name} ${surname}
 Colore preferito: ${color}
 Numero 1: ${number1}
 Numero 2: ${number2}
+Divisione tra numero 1 e numero 2: ${DivisionBetweenNumbers}
+La tua password è: ${password}
 -------------------------------------------------------------
 `;
 
